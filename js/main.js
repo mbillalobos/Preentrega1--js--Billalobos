25ee39d23c0b2b6c6e15cf17dbb8a3995c713cb6
mbillalobos <manuelignaciobillalobos@gmail.com>
2023-04-11 00:17:29 -0300

    Arreglando errores

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ alert("Bienvenido a la calculadora de la ley de Ohm")
 
 //Variables
 
-let formula = prompt("Ingrese la formula que desea calcular (Voltaje, Corriente o Resistencia)").toLowerCase();
+let formula = prompt("Escriba una de las opciones que desea calcular: Voltaje, Corriente o Resistencia").toLowerCase();
 let voltaje = "";
 let corriente = "";
 let resistencia = "";
@@ -29,16 +29,16 @@ function calcularCorriente(){
 //Condicionales
 
 if(formula == "resistencia"){
-    voltaje = parseInt(prompt("Ingrese el valor del voltaje (En Voltios)"));
-    corriente = parseInt(prompt("Ingrese el valor de la corriente eléctrica (En Amperes)"));
+    voltaje = parseInt(prompt("Ingrese, con valores numéricos, el valor del voltaje (En Voltios)"));
+    corriente = parseInt(prompt("Ingrese, con valores numéricos, el valor de la corriente eléctrica (En Amperes)"));
     alert("La resistencia eléctrica necesaria para este circuito es de: "+calcularResistencia().toFixed(2)+" Ω");
 } else if(formula == "voltaje"){
-    resistencia = parseInt(prompt("Ingrese el valor de la resistencia (En Ohm)"));
-    corriente = parseInt(prompt("Ingrese el valor de la corriente eléctrica (En Amperes)"));
+    resistencia = parseInt(prompt("Ingrese, con valores numéricos, el valor de la resistencia (En Ohm)"));
+    corriente = parseInt(prompt("Ingrese, con valores numéricos, el valor de la corriente eléctrica (En Amperes)"));
     alert("El voltaje necesario para este circuito es de: "+calcularVoltaje().toFixed(2)+" Voltios");
 } else if(formula == "corriente"){
-    resistencia = parseInt(prompt("Ingrese el valor de la resistencia (En Ohm)"));
-    voltaje = parseInt(prompt("Ingrese el valor del voltaje (En Voltios)"));
+    resistencia = parseInt(prompt("Ingrese, con valores numéricos, el valor de la resistencia (En Ohm)"));
+    voltaje = parseInt(prompt("Ingrese, con valores numéricos, el valor del voltaje (En Voltios)"));
     alert("La corriente eléctrica necesaria para este circuito es de: "+calcularCorriente().toFixed(2)+" Amperes");
 }
 
