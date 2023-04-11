d5965ca8beff1e04a39d4c8ba25377227838f70f
mbillalobos <manuelignaciobillalobos@gmail.com>
2023-04-11 00:41:02 -0300

    Arreglando todavía más errores

--- js/main.js
+++ js/main.js
@@ -9,36 +9,60 @@ let resistencia = "";
 
 //Ciclos
 
-while(formula != "voltaje" && formula != "corriente" && formula != "resistencia"){
+while (formula != "voltaje" && formula != "corriente" && formula != "resistencia") {
     alert("La fórmula ingresada es incorrecta, por favor, vuelva a ingresarla.");
     formula = prompt("Ingrese la formula que desea calcular (Voltaje, Corriente o Resistencia)").toLowerCase();
 }
 
 //Funciones
 
-function calcularVoltaje(){
+function calcularVoltaje() {
     return corriente * resistencia;
 }
-function calcularResistencia(){
+function calcularResistencia() {
     return voltaje / corriente;
 }
-function calcularCorriente(){
+function calcularCorriente() {
     return voltaje / resistencia;
 }
 
 //Condicionales
 
-if(formula == "resistencia"){
+if (formula == "resistencia") {
     voltaje = parseInt(prompt("Ingrese, con valores numéricos, el valor del voltaje (En Voltios)"));
+    while (isNaN(voltaje)) {
+        alert("El valor ingresado es incorrecto, por favor, vuelva a ingresarlo.");
+        voltaje = parseInt(prompt("Ingrese, con valores numéricos, el valor del voltaje (En Voltios)"));
+    }
     corriente = parseInt(prompt("Ingrese, con valores numéricos, el valor de la corriente eléctrica (En Amperes)"));
-    alert("La resistencia eléctrica necesaria para este circuito es de: "+calcularResistencia().toFixed(2)+" Ω");
-} else if(formula == "voltaje"){
+    while (isNaN(corriente)) {
+        alert("El valor ingresado es incorrecto, por favor, vuelva a ingresarlo.");
+        corriente = parseInt(prompt("Ingrese, con valores numéricos, el valor de la corriente eléctrica (En Amperes)"));
+    }
+    alert("La resistencia eléctrica necesaria para este circuito es de: " + calcularResistencia().toFixed(2) + " Ω");
+} else if (formula == "voltaje") {
     resistencia = parseInt(prompt("Ingrese, con valores numéricos, el valor de la resistencia (En Ohm)"));
+    while (isNaN(resistencia)) {
+        alert("El valor ingresado es incorrecto, por favor, vuelva a ingresarlo.");
+        resistencia = parseInt(prompt("Ingrese, con valores numéricos, el valor de la resistencia (En Ohm)"));
+    }
     corriente = parseInt(prompt("Ingrese, con valores numéricos, el valor de la corriente eléctrica (En Amperes)"));
-    alert("El voltaje necesario para este circuito es de: "+calcularVoltaje().toFixed(2)+" Voltios");
-} else if(formula == "corriente"){
+    while (isNaN(corriente)) {
+        alert("El valor ingresado es incorrecto, por favor, vuelva a ingresarlo.");
+        corriente = parseInt(prompt("Ingrese, con valores numéricos, el valor de la corriente eléctrica (En Amperes)"));
+    }
+    alert("El voltaje necesario para este circuito es de: " + calcularVoltaje().toFixed(2) + " Voltios");
+} else if (formula == "corriente") {
     resistencia = parseInt(prompt("Ingrese, con valores numéricos, el valor de la resistencia (En Ohm)"));
+    while (isNaN(resistencia)) {
+        alert("El valor ingresado es incorrecto, por favor, vuelva a ingresarlo.");
+        resistencia = parseInt(prompt("Ingrese, con valores numéricos, el valor de la resistencia (En Ohm)"));
+    }
     voltaje = parseInt(prompt("Ingrese, con valores numéricos, el valor del voltaje (En Voltios)"));
-    alert("La corriente eléctrica necesaria para este circuito es de: "+calcularCorriente().toFixed(2)+" Amperes");
+    while (isNaN(voltaje)) {
+        alert("El valor ingresado es incorrecto, por favor, vuelva a ingresarlo.");
+        voltaje = parseInt(prompt("Ingrese, con valores numéricos, el valor del voltaje (En Voltios)"));
+    }
+    alert("La corriente eléctrica necesaria para este circuito es de: " + calcularCorriente().toFixed(2) + " Amperes");
 }
 
